cbc558f17641dc021da748708574aabc9667db7a
ali <pokerobuka@gmail.com>
2019-05-26 22:03:14 +0200

    deck shuffle function for 8 deck

--- scripta.js
+++ scripta.js
@@ -5,25 +5,37 @@ var deck = [1,1,1,1,2,2,2,
 2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,
 "J","J","J","J","Q","Q","Q","Q","K","K","K","K"];
 
-
-function shuffle() {
+var deck8 = replicate (deck,8);
 
 var shuffled = [];
 
-while (deck.length>1) {
-var x = Math.floor(Math.random() * Math.floor(deck.length));
-shuffled.push(deck[x]);
 
-console.log(x, " index   ", deck[x]);
-deck.splice(x,1);
 
+
+function shuffle() {
+
+while (deck8.length>0) 
+
+{
+var x = Math.floor(Math.random() * Math.floor(deck8.length));
+shuffled.push(deck8[x]);
+deck8.splice(x,1);
+
+}
+var textnode = (document.createTextNode(shuffled));     
+paragraf[0].appendChild(textnode);
+paragraf[0].style.textAlign = "center";
 }
 
 
 
-
-var textnode = (document.createTextNode(shuffled));     
-paragraf[0].appendChild(textnode);
+function replicate(arr, times) {
+    for (var parts = []; times > 0; times >>= 1) {
+        if (times & 1)
+            parts.push(arr);
+        arr = arr.concat(arr);
+    }
+    return Array.prototype.concat.apply([], parts);
 }
 
 shuffle();
